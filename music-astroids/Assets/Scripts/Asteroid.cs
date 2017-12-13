23f52293fccad27acd35641ad223d265601d13dc
Perry Berman <R3alCl0ud@gmail.com>
2017-12-13 12:39:31 -0700

    add game assets

---
+++ music-astroids/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+
+namespace Assets.Scripts {
+    public class Asteroid : MovingObject {
+
+        public GameObject asteroid;
+
+        new void Start() {
+            ((MovingObject) this).Start();
+            rb.AddForce(transform.up * 9f);
+        }
+
+        public void setMass(int mass) {
+            rb.mass = mass;
+        }
+
+        new void Update() {
+            ((MovingObject) this).Update();
+        }
+
+        public void Exploded() {
+            if (rb.mass < 0.05f) {
+                DestroyImmediate(gameObject);
+            }
+        }
+    }
+}
---
+++ music-astroids/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts {
+    public class MovingObject : MonoBehaviour {
+
+
+        public Rigidbody2D rb;
+
+        public void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        public void Update() {
+            if (transform.position.x > -0.25f) {
+                transform.Translate(-17, 0, 0, Space.World);
+            }
+            if (transform.position.x < -17.5f) {
+                transform.Translate(17, 0, 0, Space.World);
+            }
+            if (transform.position.y > 10.25f) {
+                transform.Translate(0, -11, 0, Space.World);
+            }
+            if (transform.position.y < -0.75f) {
+                transform.Translate(0, 11, 0, Space.World);
+            }
+        }
+
+    }
+}
